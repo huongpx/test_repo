5a0723c3d56138efa568927fe9708f4efaf8f56e
Huong Pham <huong.px01@gmail.com>
2022-02-25 13:17:53 +0700

    add some text: 13:17

--- commit_and_push.sh
+++ commit_and_push.sh
@@ -3,6 +3,6 @@ cd /home/huongpx/Python/test_repo
 echo "Add text at $(date +'%H:%M %d/%m')" >> text.txt
 git add .
 git commit -m "add some text: $(date +'%H:%M')"
-git push -u origin master
+git push
 echo "Done"
 
